37ffe12ddfcac03ba522e4f0d77e4f667f2f50c0
vitropez <64587299+vitropez@users.noreply.github.com>
2021-06-26 12:29:24 +0200

    proyecto casi  terminado faltan comentarios

--- Geolocation.js
+++ Geolocation.js
@@ -1,32 +1,37 @@
+
 function iniciar(){
     var boton=document.getElementById('obtener');
     boton.addEventListener('click', obtener, false);
 }
-//function obtener() {
-//    navigator.geolocation.getCurrentPosition(exito, errores);
-//}
-
-//    function exito(posicion) {
 function obtener() {
+   navigator.geolocation.getCurrentPosition(exito, errores);
+}
 
-        document.getElementById('fakeMap').style.visibility = 'visible';
-        //var mapurl='https://maps.googleapis.com/maps/api/staticmap?center='+
-            //posicion.coords.latitude+','+posicion.coords.longitude+'&zoom=14&size=400x400&key=API_KEY';
+    function exito(posicion){
+
+    var mapaURL_1 = 'http://maps.google.com/maps?hl=es&ie=UTF8&q=' +posicion.coords.latitude + ',' + posicion.coords.longitude + '&z=14&output=embed';
+
+
+        document.getElementById('map').src = mapaURL_1;
+
+    document.getElementById('coordenadas1').innerHTML = posicion.coords.latitude;
+        document.getElementById('coordenadas2').innerHTML = posicion.coords.longitude;
+
+
+
 
 
     }
+    function errores(e) {
+    if (e.code == error.PERMISION_DENIED) {
+           alert("El usuario bloqueó el acceso");
+            alert("Servicio indisponible temporalmente");
+} else if (e.code == error.TIMEOUT) {
+            alert("Se sobrepasó el tiempo de espera");
+       } else {
+           alert("Error desconocido");
+        }
 
- //   function errores(e) {
-//     if (e.code == error.PERMISION_DENIED) {
-//            alert("El usuario bloqueó el acceso");
-//        } else if (e.code == error.POSITION_UNAVAILABLE) {
-//            alert("Servicio indisponible temporalmente");
-//} else if (e.code == error.TIMEOUT) {
-//            alert("Se sobrepasó el tiempo de espera");
-//        } else {
-//           alert("Error desconocido");
-//        }
-
-//}
+}
 
 window.addEventListener("load",iniciar,false) ;
